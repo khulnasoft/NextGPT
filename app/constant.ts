8fc96b496bd8afc86a2fc04100f87dbc44c2e70a
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2025-01-10 14:46:53 +0600

    feat: use regex patterns for vision models and allow adding capabilities to models through env var NEXT_PUBLIC_VISION_MODELS.
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- app/constant.ts
+++ app/constant.ts
@@ -290,6 +290,19 @@ export const DEFAULT_TTS_VOICES = [
   "nova",
   "shimmer",
 ];
+export const VISION_MODEL_REGEXES = [
+  /vision/,
+  /gpt-4o/,
+  /claude-3/,
+  /gemini-1\.5/,
+  /gemini-exp/,
+  /learnlm/,
+  /qwen-vl/,
+  /qwen2-vl/,
+  /gpt-4-turbo(?!.*preview)/, // Matches "gpt-4-turbo" but not "gpt-4-turbo-preview"
+  /^dall-e-3$/, // Matches exactly "dall-e-3"
+];
+export const EXCLUDE_VISION_MODEL_REGEXES = [/claude-3-5-haiku-20241022/];
 
 const openaiModels = [
   "gpt-3.5-turbo",
